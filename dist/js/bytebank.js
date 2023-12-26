a0f7b5c9d337dcf19c10bda2e048dddbff33b478
jorge-rufino <jorge.rufino1986@gmail.com>
2023-12-26 19:25:23 -0300

    Organizando arquivos e alterando tsconfig

--- dist/js/bytebank.js
+++ dist/js/bytebank.js
@@ -1,18 +1,19 @@
-var saldo = 6000;
-var elementoSaldo = document.querySelector('.saldo-valor .valor'); //Pega o elemento da classe ".valor" que está dentro de ".saldo-valor"
+"use strict";
+let saldo = 6000;
+const elementoSaldo = document.querySelector('.saldo-valor .valor'); //Pega o elemento da classe ".valor" que está dentro de ".saldo-valor"
 if (elementoSaldo != null) {
     elementoSaldo.textContent = saldo.toString();
 }
-var elementoFormulario = document.querySelector('.block-nova-transacao form'); //Pega o "form" que está dentro da classe ".block-nova-transacao"
+const elementoFormulario = document.querySelector('.block-nova-transacao form'); //Pega o "form" que está dentro da classe ".block-nova-transacao"
 elementoFormulario.addEventListener('submit', function (event) {
     event.preventDefault(); //Toda vez que submete um formulario, ele recarrega a página, essa linha preveni isso
-    var inputTipoTransacao = elementoFormulario.querySelector('#tipoTransacao');
-    var inputValor = elementoFormulario.querySelector('#valor');
-    var inputData = elementoFormulario.querySelector('#data');
+    const inputTipoTransacao = elementoFormulario.querySelector('#tipoTransacao');
+    const inputValor = elementoFormulario.querySelector('#valor');
+    const inputData = elementoFormulario.querySelector('#data');
     //Tipando as variaveis
-    var tipoTransacao = inputTipoTransacao.value;
-    var valor = inputValor.valueAsNumber;
-    var data = new Date(inputData.value);
+    let tipoTransacao = inputTipoTransacao.value;
+    let valor = inputValor.valueAsNumber;
+    let data = new Date(inputData.value);
     //Se os elementos não tiverem preenchidos ou se o valor for 0
     if (!elementoFormulario.checkValidity() || valor == 0) {
         alert('Por favor, preencha todos os campos da transação');
@@ -29,7 +30,7 @@ elementoFormulario.addEventListener('submit', function (event) {
         return;
     }
     elementoSaldo.textContent = saldo.toString();
-    var novaTransacao = {
+    const novaTransacao = {
         tipoTransacao: tipoTransacao,
         valor: valor,
         data: data
