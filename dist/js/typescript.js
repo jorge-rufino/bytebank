26d0a503aa754b129377be01b0a3a7f8a1c2cba3
jorge-rufino <jorge.rufino1986@gmail.com>
2023-12-26 19:44:49 -0300

    Tipos primitivos e alterando versao JS no tsconfig

---
+++ dist/js/typescript.js
@@ -0,0 +1,7 @@
+"use strict";
+//Tipos Primitivos
+let nome = "Jorge";
+let numero = 1234;
+let booleano = false;
+let qualquerCoisa = "teste";
+qualquerCoisa = 12345; //Tipo "any" é o padrão do Javascript, uma váriavel que pode receber qualquer coisa
---
+++ src/typescript.ts
@@ -0,0 +1,6 @@
+//Tipos Primitivos
+let nome: string = "Jorge";
+let numero: number = 1234;
+let booleano: boolean = false;
+let qualquerCoisa: any = "teste";
+qualquerCoisa = 12345;            //Tipo "any" é o padrão do Javascript, uma váriavel que pode receber qualquer coisa
